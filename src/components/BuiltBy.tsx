59c2e788f87e1becf9e53a663eb2878ce03db2bf
NAILUY <159585836+Nailuy@users.noreply.github.com>
2026-05-22 17:48:46 +0300

    Clean up
    Removed BuiltBy component.

--- src/components/BuiltBy.tsx
+++ src/components/BuiltBy.tsx
@@ -6,9 +6,7 @@ import dr1mmerrAvatar from '../assets/dr1mmerr.jpg';
 
 const team = [
     { name: 'nailuy', avatar: nailuyAvatar, link: 'https://x.com/yulianvakh' },
-    { name: 'vadim_kesha', avatar: keshaAvatar, link: null },
-    { name: 'pushkin_ievgen', avatar: pushkinAvatar, link: null },
-    { name: 'dr1mmerr', avatar: dr1mmerrAvatar, link: null },
+
 ];
 
 function BuiltBy() {
